11d7aca17d13df3ad5f3c4221b4004fb1ef6293b
CreatedbyRamy <120383024+CreatedbyRamy@users.noreply.github.com>
2024-12-19 13:40:03 +0100

    Update flux.sh

--- flux.sh
+++ flux.sh
@@ -6,7 +6,7 @@
 
 # Packages are installed after nodes so we can fix them...
 
-DEFAULT_WORKFLOW="https://raw.githubusercontent.com/CreatedbyRamy/ComfyUIRR/refs/heads/main/Renaissance%20Rays%20-%20Image%20generation%202.json"
+DEFAULT_WORKFLOW="https://raw.githubusercontent.com/CreatedbyRamy/ComfyUIRR/refs/heads/main/Imag_gen.json"
 
 APT_PACKAGES=(
     #"package-1"
